c2d107cd598062a7f3de300c898749dc290ab338
Barış Şenyerli <58127640+BarisX@users.noreply.github.com>
2019-12-03 03:42:50 +0300

    Add files via upload

---
+++ BarChart.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barchart;
+
+/**
+ *
+ * @author Barış
+ */
+public class BarChart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] array = { 0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+        
+        System.out.println( "Grade distribution" );
+        
+        // for each array element, output a bar of the chart
+        for( int counter = 0; counter < array.length; counter++ )
+        {
+            //output bar label ("00-09: ", ...,"90-99: ", "100: ")
+            if( counter == 10 )
+                System.out.printf( "%5d: ", 100);
+                        else
+                System.out.printf( "%02d-%02d: ", counter * 10, counter * 10 + 9 );
+            
+            for( int stars = 0; stars < array[counter]; stars++ )
+                System.out.print( "*" );
+            System.out.println();
+            
+        }
+    }
+    
+}
